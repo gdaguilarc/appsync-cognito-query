68ed62cf1370d51334b671adcd85e37a25c1566b
gdaguilarc <gd.aguilarc@gmail.com>
2019-07-03 12:51:09 -0500

    Coveralls

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # appsync-cognito-query
+
+[![Build Status](https://travis-ci.org/gdaguilarc/appsync-cognito-query.svg?branch=master)](https://travis-ci.org/gdaguilarc/appsync-cognito-query)
+
 A plugin to make easier the queries to appsync with a cognito auth user pool
---
+++ src/variables.ts
@@ -0,0 +1,26 @@
+export const authCredentials = {
+  clientId: '4t0g494erfhjvrt7eisuaf6dsd',
+  password: 'WhatsApp:1234',
+  userPoolId: 'us-west-2_64nwFO9M3',
+  username: 'WhatsApp',
+};
+
+export const endpoint = 'https://wqclvwlqwvhxbgpyyreiddegna.appsync-api.us-west-2.amazonaws.com/graphql';
+
+export const mutation = (id: string) => {
+  return `
+        mutation CreateMessage {
+          createMessage(input: {
+            messageChatId:"5523456789",
+            id: "${id}",
+            content:"Message content",
+            createdAt: "2019-07-01T20:26:57.761Z",
+            type: "TEXT"
+          }) {
+          id
+          content
+          type
+          createdAt
+      }
+}`;
+};
