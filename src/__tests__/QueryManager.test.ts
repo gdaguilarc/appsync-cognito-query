1d45113350be0e943248fb668a03b60924eb9139
gdaguilarc <gd.aguilarc@gmail.com>
2019-07-03 12:14:48 -0500

    Testing basic functionality

---
+++ src/__tests__/QueryManager.test.ts
@@ -0,0 +1,36 @@
+import { v4 as uuid } from 'uuid';
+import QueryManager from '../index';
+import { authCredentials, endpoint, mutation } from '../variables';
+
+test('Auth QueryManager', async () => {
+  const manager = new QueryManager(endpoint);
+  expect(
+    typeof (await manager.authenticateUser(
+      authCredentials.userPoolId,
+      authCredentials.clientId,
+      authCredentials.username,
+      authCredentials.password,
+    )),
+  ).toBe('string');
+});
+
+test('Mutation', async () => {
+  const manager = new QueryManager(endpoint);
+  await manager.authenticateUser(
+    authCredentials.userPoolId,
+    authCredentials.clientId,
+    authCredentials.username,
+    authCredentials.password,
+  );
+
+  const id = uuid();
+  const query = mutation(id);
+  expect(await manager.query(query)).toStrictEqual({
+    createMessage: {
+      content: 'Message content',
+      createdAt: '2019-07-01T20:26:57.761Z',
+      id,
+      type: 'TEXT',
+    },
+  });
+});
